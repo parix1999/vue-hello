a9376c214cc23d67a20dcf57650a52873a4c6b62
parix1999 <parisottoedoardo@gmail.com>
2021-06-24 15:23:01 +0200

    js-1.0

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+          //Avviare Vue:
+const app = new Vue (
+    {
+        //Dichciare in quale contenitore inserirlo
+        el:'#app',
+        //-----------
+        //Inserimento dei dati quindi le nostre variabili utilizzabii
+        data:{
+            text:'',
+            contentClass: 'larghezza',
+        }
+    }
+);
